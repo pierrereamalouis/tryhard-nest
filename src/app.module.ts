dca8fa8eae02cd33ff99ec7ba54925124a98f2bd
Louis <PRT_pierre-reama.louis@cgi.com>
2021-05-24 23:32:57 -0400

    connection with postgres and typeorm established

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import configuration from './config/configuration';
-import { typeOrmConfig } from './config/typeorm.config';
+import { getConnectionOptions } from 'typeorm';
 import { LeagueModule } from './league/league.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-      isGlobal: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
     }),
-    TypeOrmModule.forRoot(typeOrmConfig),
     LeagueModule,
   ],
 })
-export class AppModule {
-  constructor(private configService: ConfigService) {}
-}
+export class AppModule {}
--- src/config/configuration.ts
+++
@@ -1,9 +0,0 @@
-export default () => ({
-  database: {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS,
-    name: process.env.DB_NAME,
-  },
-});
