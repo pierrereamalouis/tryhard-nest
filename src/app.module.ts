786aa747eaba01d55a36fb70417d6cc683239fd7
prlouis <pierrereama.louis@gmail.com>
2021-09-06 21:54:52 -0400

    Create vue app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,14 @@ import { PoolModule } from './pool/pool.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MailModule } from './mail/mail.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+    }),
     TypeOrmModule.forRootAsync({
       useFactory: async () =>
         Object.assign(await getConnectionOptions(), {
